8bff70b17052960d697b9ebbcc81fcd235513c29
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2023-05-07 23:40:24 +0100

    Uses NavigationStack's data and destination builders

--- Sources/NavigationBackport/DestinationBuilderModifier.swift
+++ Sources/NavigationBackport/DestinationBuilderModifier.swift
@@ -2,15 +2,24 @@ import Foundation
 import SwiftUI
 
 /// Modifier for appending a new destination builder.
-struct DestinationBuilderModifier<TypedData>: ViewModifier {
+struct DestinationBuilderModifier<TypedData: Hashable>: ViewModifier {
   let typedDestinationBuilder: DestinationBuilder<TypedData>
+  @Environment(\.useNavigationStack) var useNavigationStack
 
   @EnvironmentObject var destinationBuilder: DestinationBuilderHolder
 
   func body(content: Content) -> some View {
     destinationBuilder.appendBuilder(typedDestinationBuilder)
-
-    return content
-      .environmentObject(destinationBuilder)
+    if #available(iOS 16.0, *), useNavigationStack == .whenAvailable {
+      return AnyView(
+        content
+          .navigationDestination(for: TypedData.self, destination: { typedDestinationBuilder($0) })
+      )
+    } else {
+      return AnyView(
+        content
+          .environmentObject(destinationBuilder)
+      )
+    }
   }
 }
--- Sources/NavigationBackport/NBNavigationStack.swift
+++ Sources/NavigationBackport/NBNavigationStack.swift
@@ -9,35 +9,72 @@ public struct NBNavigationStack<Root: View, Data: Hashable>: View {
   @StateObject var path = NavigationPathHolder()
   @StateObject var pathAppender = PathAppender()
   @StateObject var destinationBuilder = DestinationBuilderHolder()
+  @Environment(\.useNavigationStack) var useNavigationStack
   var root: Root
   var useInternalTypedPath: Bool
+
+  var requiresDelays: Bool {
+    if #available(iOS 16.0, *), useNavigationStack == .whenAvailable {
+      return false
+    } else {
+      return true
+    }
+  }
 
   var content: some View {
     pathAppender.append = { [weak path] newElement in
       path?.path.append(newElement)
     }
-    return NavigationWrapper {
-      Router(rootView: root, screens: $path.path)
+    if #available(iOS 16.0, *), useNavigationStack == .whenAvailable {
+      return AnyView(
+        NavigationStack(path: useInternalTypedPath ? $internalTypedPath : $externalTypedPath) {
+          root
+            .navigationDestination(for: AnyHashable.self, destination: { destinationBuilder.build($0) })
+            .navigationDestination(for: LocalDestinationID.self, destination: { destinationBuilder.build($0) })
+        }
+          .environment(\.isWithinNavigationStack, true)
+          .environmentObject(path)
+          .environmentObject(pathAppender)
+          .environmentObject(destinationBuilder)
+          .environmentObject(Navigator(useInternalTypedPath ? $internalTypedPath : $externalTypedPath))
+      )
     }
-    .environmentObject(path)
-    .environmentObject(pathAppender)
-    .environmentObject(destinationBuilder)
-    .environmentObject(Navigator(useInternalTypedPath ? $internalTypedPath : $externalTypedPath))
+    return AnyView(
+      NavigationWrapper {
+        Router(rootView: root, screens: $path.path)
+      }
+      .environmentObject(path)
+      .environmentObject(pathAppender)
+      .environmentObject(destinationBuilder)
+      .environmentObject(Navigator(useInternalTypedPath ? $internalTypedPath : $externalTypedPath))
+    )
   }
 
   public var body: some View {
     content
       .onFirstAppear {
+        guard requiresDelays else {
+          path.path = externalTypedPath
+          return
+        }
         path.withDelaysIfUnsupported(\.path) {
           $0 = externalTypedPath
         }
       }
       .onChange(of: externalTypedPath) { externalTypedPath in
+        guard requiresDelays else {
+          path.path = externalTypedPath
+          return
+        }
         path.withDelaysIfUnsupported(\.path) {
           $0 = externalTypedPath
         }
       }
       .onChange(of: internalTypedPath) { internalTypedPath in
+        guard requiresDelays else {
+          path.path = internalTypedPath
+          return
+        }
         path.withDelaysIfUnsupported(\.path) {
           $0 = internalTypedPath
         }
