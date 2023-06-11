0d883a2c63161252db09d2fdef5c6bfa15a7e34e
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2023-06-11 22:24:49 +0100

    Overcomes navigation stack issue, but could still be popping issue

--- Sources/NavigationBackport/View+show.swift
+++ Sources/NavigationBackport/View+show.swift
@@ -4,6 +4,14 @@ struct ShowModifier<Destination: View>: ViewModifier {
   var isActiveBinding: Binding<Bool>
   var routeStyle: RouteStyle?
   var destination: Destination
+  
+  func isActiveBinding(enabled: Bool) -> Binding<Bool> {
+    Binding {
+      enabled && isActiveBinding.wrappedValue
+    } set: {
+      isActiveBinding.wrappedValue = $0
+    }
+  }
 
   func body(content: Content) -> some View {
     /// NOTE: On iOS 14.4 and below, a bug prevented multiple sheet/fullScreenCover modifiers being chained
@@ -11,9 +19,9 @@ struct ShowModifier<Destination: View>: ViewModifier {
     /// https://developer.apple.com/documentation/ios-ipados-release-notes/ios-ipados-14_5-release-notes
     if #available(iOS 14.5, *) {
       content
-        .push(isActive: routeStyle?.isPush == true ? isActiveBinding : .constant(false), destination: destination)
-        .sheet(isActive: routeStyle?.isSheet == true ? isActiveBinding : .constant(false), destination: destination)
-        .cover(isActive: routeStyle?.isCover == true ? isActiveBinding : .constant(false), destination: destination)
+        .push(isActive: isActiveBinding(enabled: routeStyle?.isPush ?? false), destination: destination)
+        .sheet(isActive: isActiveBinding(enabled: routeStyle?.isSheet ?? false), destination: destination)
+        .cover(isActive: isActiveBinding(enabled: routeStyle?.isCover ?? false), destination: destination)
     } else {
       if routeStyle?.isSheet == true {
         content
