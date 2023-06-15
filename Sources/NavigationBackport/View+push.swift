0b07e566232d167c9d0cacd49c64ef73050752fc
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2023-06-16 00:24:21 +0100

    Uses true Binding

--- Sources/NavigationBackport/View+push.swift
+++ Sources/NavigationBackport/View+push.swift
@@ -1,21 +1,22 @@
 import SwiftUI
 
 struct PushModifier<Destination: View>: ViewModifier {
-  var isActiveBinding: Binding<Bool>
+  @Binding var isActive: Bool
   var destination: Destination
+
   @Environment(\.isWithinNavigationStack) var isWithinNavigationStack
 
   func body(content: Content) -> some View {
     if #available(iOS 16.0, *, macOS 13.0, *, watchOS 7.0, *, tvOS 14.0, *), isWithinNavigationStack {
       AnyView(
         content
-          .navigationDestination(isPresented: isActiveBinding, destination: { destination })
+          .navigationDestination(isPresented: $isActive, destination: { destination })
       )
     } else {
       AnyView(
         content
           .background(
-            NavigationLink(destination: destination, isActive: isActiveBinding, label: EmptyView.init)
+            NavigationLink(destination: destination, isActive: $isActive, label: EmptyView.init)
               .hidden()
           )
       )
@@ -25,6 +26,6 @@ struct PushModifier<Destination: View>: ViewModifier {
 
 extension View {
   func push<Destination: View>(isActive: Binding<Bool>, destination: Destination) -> some View {
-    return modifier(PushModifier(isActiveBinding: isActive, destination: destination))
+    return modifier(PushModifier(isActive: isActive, destination: destination))
   }
 }
