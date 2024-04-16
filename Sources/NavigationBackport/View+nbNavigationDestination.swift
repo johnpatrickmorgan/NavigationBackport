e653da7a4c03c05fb35fd11338f23146ba992c0e
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2024-04-16 17:01:08 +0100

    Ensures changing item does not push new screen

--- Sources/NavigationBackport/View+nbNavigationDestination.swift
+++ Sources/NavigationBackport/View+nbNavigationDestination.swift
@@ -55,20 +55,73 @@ public extension View {
       )
     )
   }
-}
 
-public extension View {
   @available(iOS, deprecated: 16.0, message: "Use SwiftUI's Navigation API beyond iOS 15")
-  /// Associates a destination view with a bound value for use within
-  /// a ``NBNavigationStack``.
+  /// Associates a destination view with a binding that can be used to push
+  /// the view onto a ``NBNavigationStack``.
   ///
-  /// Add this view modifer to a view inside a ``NBNavigationStack`` to describe 
-  /// the view that the stack displays when presenting a particular kind of data.
-  /// Programmatically update the binding to display or remove the view. For example,
-  /// you can replace the view showing in the detail column of a navigation stack:
+  /// In general, favor binding a path to a navigation stack for programmatic
+  /// navigation. Add this view modifer to a view inside a ``NBNavigationStack``
+  /// to programmatically push a single view onto the stack. This is useful
+  /// for building components that can push an associated view. For example,
+  /// you can present a `ColorDetail` view for a particular color:
+  ///
+  ///     @State private var showDetails = false
+  ///     var favoriteColor: Color
+  ///
+  ///     NBNavigationStack {
+  ///         VStack {
+  ///             Circle()
+  ///                 .fill(favoriteColor)
+  ///             Button("Show details") {
+  ///                 showDetails = true
+  ///             }
+  ///         }
+  ///         .nbNavigationDestination(isPresented: $showDetails) {
+  ///             ColorDetail(color: favoriteColor)
+  ///         }
+  ///         .nbNavigationTitle("My Favorite Color")
+  ///     }
+  ///
+  /// Do not put a navigation destination modifier inside a "lazy" container,
+  /// like ``List`` or ``LazyVStack``. These containers create child views
+  /// only when needed to render on screen. Add the navigation destination
+  /// modifier outside these containers so that the navigation stack can
+  /// always see the destination.
+  ///
+  /// - Parameters:
+  ///   - isPresented: A binding to a Boolean value that indicates whether
+  ///     `destination` is currently presented.
+  ///   - destination: A view to present.
+  func nbNavigationDestination<Data: Hashable, V>(item: Binding<Data?>, @ViewBuilder destination: @escaping (Data) -> V) -> some View where V: View {
+    let binding = Binding(
+      get: { item.wrappedValue != nil },
+      set: { isActive, transaction in
+        if !isActive {
+          item.transaction(transaction).wrappedValue = nil
+        }
+      }
+    )
+    return modifier(
+      LocalDestinationBuilderModifier(
+        isPresented: binding,
+        builder: { AnyView(ConditionalViewBuilder(data: item, buildView: destination)) }
+      )
+    )
+  }
+
+  @available(iOS, deprecated: 16.0, message: "Use SwiftUI's Navigation API beyond iOS 15")
+  /// Associates a destination view with a bound value for use within a
+  /// navigation stack.
+  ///
+  /// Add this view modifer to a view inside an ``NBNavigationStack`` to describe
+  /// the view that the stack displays when presenting a particular kind of data. Programmatically
+  /// update the binding to display or remove the view. For example, you can replace
+  /// the view showing in the detail column of a navigation split view:
   ///
   ///     @State private var colorShown: Color?
-  ///     NBNavigationStack {
+  ///
+  ///     NBNavigationView {
   ///         List {
   ///             Button("Mint") { colorShown = .mint }
   ///             Button("Pink") { colorShown = .pink }
@@ -79,22 +132,39 @@ public extension View {
   ///         }
   ///     }
   ///
+  /// When the person using the app taps on the Mint button, the mint color
+  /// is pushed onto the navigation stack. You can pop the view
+  /// by setting `colorShown` back to `nil`.
+  ///
+  /// You can add more than one navigation destination modifier to the stack
+  /// if it needs to present more than one kind of data.
+  ///
   /// Do not put a navigation destination modifier inside a "lazy" container,
   /// like ``List`` or ``LazyVStack``. These containers create child views
   /// only when needed to render on screen. Add the navigation destination
-  /// modifier outside these containers so that the navigation stack can
+  /// modifier outside these containers so that the navigation view can
   /// always see the destination.
   ///
   /// - Parameters:
-  ///   - item: A binding to the data presented, or nil if nothing is currently presented.
-  ///   - destination: A view builder that defines a view to display when item is not nil.
-  func nbNavigationDestination<D: Hashable, C: View>(item: Binding<D?>, @ViewBuilder destination: @escaping (D) -> C) -> some View {
-    let builtDestination = AnyView(item.wrappedValue.map(destination))
-    return modifier(
-      LocalDestinationBuilderForItemModifier(
-        item: item,
-        builder: { builtDestination }
-      )
+  ///   - item: A binding to the data presented, or `nil` if nothing is
+  ///     currently presented.
+  ///   - destination: A view builder that defines a view to display
+  ///     when `item` is not `nil`.
+  func nbNavigationDestination2<D: Hashable, C: View>(item: Binding<D?>, @ViewBuilder destination: @escaping (D) -> C) -> some View {
+    nbNavigationDestination(
+      isPresented: Binding(
+        get: { item.wrappedValue != nil },
+        set: { isActive, transaction in
+          if !isActive {
+            item.transaction(transaction).wrappedValue = nil
+          }
+        }
+      ),
+      destination: { ConditionalViewBuilder(data: item, buildView: destination) }
     )
   }
 }
+
+/// `Void` does not conform to Hashable, so we create a similar type that does. So that we can transform between
+/// a Bool Binding and a VoidHashable binding without losing hashability.
+private struct VoidHashable: Hashable {}
