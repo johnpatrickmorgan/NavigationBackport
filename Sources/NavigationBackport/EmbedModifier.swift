f9ae04868653c02f4c524cd7b93bff53311fc1d1
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2024-05-13 15:52:50 +0100

    Removes use of AnyView

--- Sources/NavigationBackport/EmbedModifier.swift
+++ Sources/NavigationBackport/EmbedModifier.swift
@@ -5,13 +5,14 @@ struct EmbedModifier: ViewModifier {
   var withNavigation: Bool
   @Environment(\.useNavigationStack) var useNavigationStack
 
+  @ViewBuilder
   func wrapped(content: Content) -> some View {
     if #available(iOS 16.0, *, macOS 13.0, *, watchOS 7.0, *, tvOS 14.0, *), useNavigationStack == .whenAvailable {
-      return AnyView(NavigationStack { content })
+      NavigationStack { content }
         .environment(\.isWithinNavigationStack, true)
     } else {
-      return AnyView(NavigationView { content }
-        .navigationViewStyle(supportedNavigationViewStyle))
+      NavigationView { content }
+        .navigationViewStyle(supportedNavigationViewStyle)
         .environment(\.isWithinNavigationStack, false)
     }
   }
