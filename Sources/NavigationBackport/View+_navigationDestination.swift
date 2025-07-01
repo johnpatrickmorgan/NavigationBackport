5baf99855f6a0a02022f9b479cc41ad99a8e070c
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2025-07-02 00:07:10 +0100

    Removes unnecessary AnyView wrapper

--- Sources/NavigationBackport/View+_navigationDestination.swift
+++ Sources/NavigationBackport/View+_navigationDestination.swift
@@ -7,18 +7,14 @@ struct NavigationLinkModifier<Destination: View>: ViewModifier {
 
   func body(content: Content) -> some View {
     if #available(iOS 16.0, *, macOS 13.0, *, watchOS 9.0, *, tvOS 16.0, *), isWithinNavigationStack {
-      AnyView(
         content
           .navigationDestination(isPresented: $isActiveBinding, destination: { destination })
-      )
     } else {
-      AnyView(
         content
           .background(
             NavigationLink(destination: destination, isActive: $isActiveBinding, label: EmptyView.init)
               .hidden()
           )
-      )
     }
   }
 }
